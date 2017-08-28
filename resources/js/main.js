edc8fb27f552a0f90ec46680218c6f1b14709167
Matej Pavlík <Snip3rSVK@gmail.com>
2017-08-29 00:10:21 +0200

    pre release

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,8 @@
+let colors = getJSON("https://raw.githubusercontent.com/Snip3rSVK/Random-quote/master/resources/json/colors.json", (data) => colors = data);
 let quotes = getJSON("https://raw.githubusercontent.com/Snip3rSVK/Random-quote/master/resources/json/quotes.json", (data) => {
 	quotes = data;
 	generateQuote();
 });
-let colors = getJSON("https://raw.githubusercontent.com/Snip3rSVK/Random-quote/master/resources/json/colors.json", (data) => colors = data);
 
 const newQuoteBtn = document.querySelector("#new-quote-btn");
 const quote = document.querySelector("#quote");
@@ -23,11 +23,13 @@ function getJSON(url, callback) {
 }
 
 function generateQuote() {
-	const randomQuoteNum = Math.floor(Math.random() * (quotes.length - 1));
 	const randomColorNum = Math.floor(Math.random() * (colors.length - 1));
+	const randomQuoteNum = Math.floor(Math.random() * (quotes.length - 1));
+	console.log(quote);
+	console.log(colors[randomColorNum]);
+	quote.style = "background: " + colors[randomColorNum];
 	quote.textContent = quotes[randomQuoteNum][0];
 	author.textContent = quotes[randomQuoteNum][1];
-	quote.style.backgroundColor = colors[randomColorNum];
 }
 
 newQuoteBtn.addEventListener("click", generateQuote);
