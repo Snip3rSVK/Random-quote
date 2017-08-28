a10011a88e3e143436150f6d20f56ae055130fd9
Matej Pavlík <Snip3rSVK@gmail.com>
2017-08-28 23:58:13 +0200

    pre release

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,23 +1,36 @@
-(function() {
-	let httpRequest;
+let quotes = getJSON("https://raw.githubusercontent.com/Snip3rSVK/Random-quote/master/resources/json/quotes.json", (data) => {
+	quotes = data;
+	generateQuote();
+});
+let colors = getJSON("https://raw.githubusercontent.com/Snip3rSVK/Random-quote/master/resources/json/colors.json", (data) => colors = data);
 
-	function makeRequest() {
-		httpRequest = new XMLHttpRequest();
+const newQuoteBtn = document.querySelector("#new-quote-btn");
+const quote = document.querySelector("#quote");
+const author = document.querySelector("#author");
 
-		if (!httpRequest) {
-			return false;
-		}
+function getJSON(url, callback) {
+	const httpRequest = new XMLHttpRequest();
 
-		httpRequest.overrideMimeType("application/json");
-		httpRequest.onreadystatechange = readJSON;
-		httpRequest.open("GET", "B:/Snip3r/Programing, Coding, Scripting/HTML & CSS & JavaScript/Random quote/JSON/quotes.json");
-		httpRequest.send(null);
-	}
+	if (!httpRequest)
+		return false;
 
-	function readJSON() {
-		if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-			console.log(JSON.parse(httpRequest.responseText));
-		}
-	}
+	httpRequest.open("GET", url);
+	httpRequest.onreadystatechange = function() {
+		if (httpRequest.readyState === 4 && httpRequest.status === 200)
+			callback(JSON.parse(httpRequest.responseText));
+	};
+	httpRequest.send();
+}
 
-})();
+function generateQuote() {
+	const randomQuoteNum = Math.floor(Math.random() * (quotes.length - 1));
+	const randomColorNum = Math.floor(Math.random() * (colors.length - 1));
+	quote.textContent = quotes[randomQuoteNum][0];
+	author.textContent = quotes[randomQuoteNum][1];
+	quote.style.backgroundColor = colors[randomColorNum];
+}
+
+newQuoteBtn.addEventListener("click", generateQuote);
+
+
+
